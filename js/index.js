b53b51c965287fb095cad297ae9474b827adacac
Oleksandra Serkes <alexsandra.serkes@gmail.com>
2023-05-17 15:23:03 +0300

    Changed font-family for logo and renamed functions in index.js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-const openModal = document.getElementById('open');
-const closeModal = document.getElementById('close');
+const openModalBtn = document.getElementById('open');
+const closeModalBtn = document.getElementById('close');
 const modal = document.getElementById('modal');
 const menu = document.querySelector('.modal__nav-menu');
 const overlay = document.querySelector('.overlay');
 
-const open = function () {
+const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const close = function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-openModal.addEventListener('click', open);
+openModalBtn.addEventListener('click', open);
 
-closeModal.addEventListener('click', close);
+closeModalBtn.addEventListener('click', close);
 
 overlay.addEventListener('click', close);
 
